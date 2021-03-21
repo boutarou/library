ddde97fbda869dc158e19d61f4c27a914db8c9ff
boutarou <araarahirao@gmail.com>
2021-03-21 12:14:35 +0900

    [Add] template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for(int i = 0; i < int(n); i++)
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    
+    return 0;
+}
